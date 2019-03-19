61c290f796b9db469ffccdf15249513dac16dd20
Alfreds Genkins <alfreds@scandiweb.com>
2019-03-20 01:38:14 +0200

    Implemented functional

--- src/Model/Resolver/CreateCustomer.php
+++ src/Model/Resolver/CreateCustomer.php
@@ -13,24 +13,28 @@ declare(strict_types=1);
 
 namespace ScandiPWA\CustomerGraphQl\Model\Resolver;
 
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Newsletter\Model\SubscriberFactory;
+use Magento\Framework\Api\DataObjectHelper;
+use Magento\Customer\Api\AccountManagementInterface;
 use Magento\Framework\App\ObjectManager;
-use Magento\Framework\Exception\InputException;
-use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\Exception\StateException;
+use Magento\Integration\Api\CustomerTokenServiceInterface;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Customer\Model\Session;
 use Magento\Customer\Model\Registration;
 use Magento\Framework\Data\Form\FormKey\Validator;
-use Magento\Customer\Model\CustomerFactory;
 use Magento\Store\Model\StoreManagerInterface;
-use Magento\Customer\Model\AddressFactory;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Framework\GraphQl\Config\Element\Field;
-use Magento\Framework\GraphQl\Query\Resolver\Value;
-use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Customer\Api\Data\CustomerInterfaceFactory;
+use Magento\Customer\Api\GroupManagementInterface;
+use Magento\CustomerGraphQl\Model\Customer\CustomerDataProvider;
 
 class CreateCustomer implements ResolverInterface {
+    const REGISTRATION_STATUS_SUCCESS = 'success';
+    const REGISTRATION_STATUS_PENDING_CONFIRMATION = 'pending_confirmation';
+
     /**
      * @var \Magento\Customer\Model\Registration
      */
@@ -46,31 +50,116 @@ class CreateCustomer implements ResolverInterface {
      */
     private $formKeyValidator;
 
-    private $customerFactory;
-
-    private $storeManager;
+    /**
+     * @var AccountManagementInterface
+     */
+    private $accountManagement;
 
     /**
+     * @var CustomerInterfaceFactory
+     */
+    protected $customerFactory;
+
+    /**
+     * @var DataObjectHelper
+     */
+    protected $dataObjectHelper;
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * @var GroupManagementInterface
+     */
+    protected $customerGroupManagement;
+
+    /**
+     * @var SubscriberFactory
+     */
+    protected $subscriberFactory;
+
+    /**
+     * @var CustomerTokenServiceInterface
+     */
+    private $customerTokenService;
+
+    /**
+     * @var CustomerRepositoryInterface
+     */
+    private $customerRepository;
+
+    /**
+     * @var CustomerDataProvider
+     */
+    private $customerDataProvider;
+
+    /**
+     * CreateCustomer constructor.
      * @param Session $customerSession
      * @param Registration $registration
-     * @param Validator $formKeyValidator
-     *
-     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     * @param SubscriberFactory $subscriberFactory
+     * @param AccountManagementInterface $accountManagement
+     * @param CustomerInterfaceFactory $customerFactory
+     * @param DataObjectHelper $dataObjectHelper
+     * @param StoreManagerInterface $storeManager
+     * @param GroupManagementInterface $customerGroupManagement
+     * @param CustomerTokenServiceInterface $customerTokenService
+     * @param CustomerRepositoryInterface $customerRepository
+     * @param Validator|null $formKeyValidator
      */
     public function __construct(
         Session $customerSession,
         Registration $registration,
-        CustomerFactory $customerFactory,
+        SubscriberFactory $subscriberFactory,
+        AccountManagementInterface $accountManagement,
+        CustomerInterfaceFactory $customerFactory,
+        DataObjectHelper $dataObjectHelper,
         StoreManagerInterface $storeManager,
-        AddressFactory $addressFactory,
+        GroupManagementInterface $customerGroupManagement,
+        CustomerTokenServiceInterface $customerTokenService,
+        CustomerRepositoryInterface $customerRepository,
+        CustomerDataProvider $customerDataProvider,
         Validator $formKeyValidator = null
     ) {
-        $this->storeManager = $storeManager;
+        $this->customerTokenService = $customerTokenService;
+        $this->dataObjectHelper = $dataObjectHelper;
         $this->customerFactory = $customerFactory;
+        $this->accountManagement = $accountManagement;
+        $this->customerRepository = $customerRepository;
         $this->session = $customerSession;
         $this->registration = $registration;
-        $this->addressFactory = $addressFactory;
+        $this->subscriberFactory = $subscriberFactory;
+        $this->storeManager = $storeManager;
+        $this->customerDataProvider = $customerDataProvider;
+        $this->customerGroupManagement = $customerGroupManagement;
         $this->formKeyValidator = $formKeyValidator ?: ObjectManager::getInstance()->get(Validator::class);
+    }
+
+    public function extractCustomer($customerData) {
+        $customerDataObject = $this->customerFactory->create();
+        $this->dataObjectHelper->populateWithArray(
+            $customerDataObject,
+            $customerData,
+            \Magento\Customer\Api\Data\CustomerInterface::class
+        );
+
+        /**
+         * Here we might check for allowed attributes,
+         * like in Magento\Customer\Model\CustomerExtractor
+         * This might allow to set GroupId from request
+         */
+
+        $store = $this->storeManager->getStore();
+        $customerDataObject->setGroupId(
+            $this->customerGroupManagement->getDefaultGroup($store->getId())->getId()
+        );
+
+        $customerDataObject->setWebsiteId($store->getWebsiteId());
+        $customerDataObject->setStoreId($store->getId());
+
+        return $customerDataObject;
     }
 
     /**
@@ -84,49 +173,59 @@ class CreateCustomer implements ResolverInterface {
         array $args = null
     ) {
         if ($this->session->isLoggedIn() || !$this->registration->isAllowed()) {
-            // customer is logged in and registration is not allowed
+            throw new GraphQlInputException(__('Customer is already logged in.'));
         }
 
-//        if (!$this->formKeyValidator->validate($this->getRequest())) {
-//            // form key is invalid
-//        }
+        /**
+         * Where is my field validation ???
+         */
 
         $this->session->regenerateId();
 
         try {
+            $token = null;
+            $status = null;
+
             $customerData = $args['customer'];
-            $passwordData = $args['password'];
+            $password = $args['password'];
 
+            $customer = $this->extractCustomer($customerData);
+//            $customer->setAddresses($customerData['addresses']);
 
+            $customer = $this->accountManagement
+                ->createAccount($customer, $password);
 
-            $websiteId = $this->storeManager->getWebsite()->getWebsiteId();
-            $customer = $this->customerFactory->create();
-            $customer
-                ->setWebsiteId($websiteId)
-                ->setEmail($customerData['email'])
-                ->setFirstname($customerData['firstname'])
-                ->setLastname($customerData['lastname'])
-                ->setPassword($passwordData)
-                ->save();
+            if (!isset($customerData['is_subscribed']) || $customerData['is_subscribed']) {
+                $this->subscriberFactory->create()
+                    ->subscribeCustomerById($customer->getId());
+            }
 
-            $address = $this->addressFactory->create();
-            $address
-                ->setCustomerId($customer->getId())
-                ->setData($customerData['addresses'])
-                ->setIsDefaultBilling('1')
-                ->setIsDefaultShipping('1')
-                ->setSaveInAddressBook('1')
-                ->save();
+            /**
+             * We should potentially dispatch an 'customer_register_success'
+             * here. Not sure if it is done in GraphQL resolvers.
+             */
 
-            return $customer->getData();
-        } catch (StateException $e) {
-            throw new GraphQlInputException(__('There is already an account with this email address.'), $e);
-        } catch (InputException $e) {
-            throw new GraphQlInputException(__($e->getMessage()), $e);
-        } catch (LocalizedException $e) {
-            throw new GraphQlInputException(__($e->getMessage()), $e);
+            $confirmationStatus = $this->accountManagement->getConfirmationStatus($customer->getId());
+            if ($confirmationStatus === AccountManagementInterface::ACCOUNT_CONFIRMATION_REQUIRED) {
+                $status = self::REGISTRATION_STATUS_PENDING_CONFIRMATION;
+                /**
+                 * We need to send email or what ???
+                 */
+            } else {
+                $this->session->setCustomerDataAsLoggedIn($customer);
+                $token = $this->customerTokenService->createCustomerAccessToken($customer->getEmail(), $password);
+                $status = self::REGISTRATION_STATUS_SUCCESS;
+            }
+
+            return array_unique(
+                array_merge(
+                    $this->customerDataProvider->getCustomerById((int)$customer->getId()),
+                    [ 'status' => $status ],
+                    [ 'token' => $token ]
+                )
+            );
         } catch (\Exception $e) {
-            throw new GraphQlInputException(__('We can\'t save the customer.'), $e);
+            throw new GraphQlInputException(__($e->getMessage()), $e);
         }
     }
 }
